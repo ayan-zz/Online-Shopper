40153f7f26d267798d12d0569c1db04e44df461c
ayan-zz <ayancgowdhury00@gmail.com>
2023-06-03 21:50:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('results.html')
+
+    else:
+        data=CustomData(
+            Gender=(request.form.get('Gender')),
+            Age=float(request.form.get('Age')),
+            Height=float(request.form.get('Height')),
+            Weight=float(request.form.get('Weight')),
+            family_history_with_overweight=(request.form.get('family_history_with_overweight')),
+            FAVC=(request.form.get('FAVC')),
+            FCVC=float(request.form.get('FCVC')),
+            NCP=float(request.form.get('NCP')),
+            CAEC=(request.form.get('CAEC')),
+            SMOKE=(request.form.get('SMOKE')),
+            CH2O=float(request.form.get('CH2O')),
+            SCC=(request.form.get('SCC')),
+            FAF=float(request.form.get('FAF')),
+            TUE=float(request.form.get('TUE')),
+            CALC=(request.form.get('CALC')),
+            MTRANS=(request.form.get('MTRANS'))
+        )
+        pred_df=data.get_data_as_dataframe()
+        print(pred_df)
+
+        predict_pipeline=PredictPipeline()
+        result=predict_pipeline.predict(pred_df)
+        
+        categories={0:'INSUFFICIENT WEIGHT',1:'NORMAL WEIGHT',2:'OBESITY TYPE I',
+                    3:'OBESITY TYPE II',4:'OBESITY TYPE III',5:'OVERWEIGHT LEVEL I',6:'OVERWEIGHT LEVEL II'}
+        
+        
+        results = categories[result[0]]
+        
+
+        return render_template("results.html",results=results)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True,port=5000)
+    
+    
+     
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,45 @@
+import sys,os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from src.logging import logging
+from src.exception import CustomException
+from dataclasses import dataclass
+
+@dataclass
+class data_ingestion_config:
+    raw_path:str=os.path.join('artifacts','raw.csv')
+    train_path:str=os.path.join('artifacts','train.csv')
+    test_path:str=os.path.join('artifacts','test.csv')
+class data_ingestion:
+    def __init__(self):
+        self.ingestion_config=data_ingestion_config()
+
+    def initiate_data_ingestion(self):
+        try:
+            logging.info('data initiation started')
+            data=pd.read_csv('C:\\Users\\Lenovo\\Downloads\\ML project\\CICD online_shoppers\\notebook\\online_shoppers_intention.csv')
+            
+            logging.info('data ingested done')
+
+            logging.info('making directories for storing raw data')
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_path),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_path,index=False)
+
+            logging.info('initiating train test split on original data')
+            train_set,test_set=train_test_split(data,test_size=0.20,random_state=35)
+            train_set.to_csv(self.ingestion_config.train_path,index=False)
+            test_set.to_csv(self.ingestion_config.test_path,index=False)
+
+            logging.info('train and test data had been uploaded to artifacts')
+
+            return(self.ingestion_config.train_path,self.ingestion_config.test_path)
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+
+
+    
+
